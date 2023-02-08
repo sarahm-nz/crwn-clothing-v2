e277a47c6550eb3b086b0ae7578fc65bb9f13f16
Sarah Mays <sarah.mays@environz.co.nz>
2023-02-08 15:40:12 +1300

    adding cart context and shopping icon

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import './index.scss';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
-      </UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+          <App />
+          </CartProvider>
+        </ProductsProvider>
+      </UserProvider>      
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,18 @@
+import SHOP_DATA from '../../shop-data.json';
+import { ProductsContext } from '../../contexts/products.context';
+import { useContext } from 'react';
+import ProductCard from '../../components/product-card/product-card.component';
+import './shop.styles.scss';
+
+
+export default function Shop() {
+    const { products } = useContext(ProductsContext);
+
+    return (
+        <div className='products-container'>
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}            
+        </div>
+    )
+}
