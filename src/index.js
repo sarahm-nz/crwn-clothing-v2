f6170bc56add61e740893d5806f3075de0556f00
Sarah Mays <sarah.mays@environz.co.nz>
2023-02-09 11:30:08 +1300

    finishing touches

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import './index.scss';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 
@@ -12,11 +12,11 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>      
     </BrowserRouter>
   </React.StrictMode>,
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 import { async } from 'q';
 
 const firebaseConfig = {
@@ -21,9 +21,36 @@ const firebaseConfig = {
     });
 
     export const auth = getAuth();
-    export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+    export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);   
 
     export const db = getFirestore();
+
+    export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+        const collectionRef = collection(db, collectionKey);
+        const batch = writeBatch(db);
+
+        objectsToAdd.forEach((object) => {
+            const docRef = doc(collectionRef, object.title.toLowerCase());
+            batch.set(docRef, object);
+        });
+
+        await batch.commit();
+        console.log('done');
+    };
+
+    export const getCategoriesAndDocuments = async () => {
+        const collectionRef = collection(db, 'categories');
+        const q = query(collectionRef);
+
+        const querySnapshot = await getDocs(q);
+        const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+            const {title, items} = docSnapshot.data();
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {});
+
+        return categoryMap;
+    }
 
     export const createUserDocumentFromAuth = async (
         userAuth, 
